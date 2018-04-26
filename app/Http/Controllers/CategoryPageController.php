01e72b9a680f12ac29723f906ec625e285f92176
Adrià Salvans <adriasalvans@gmail.com>
2018-04-26 23:24:59 +0200

    14e commit

--- app/Http/Controllers/CategoryPageController.php
+++ app/Http/Controllers/CategoryPageController.php
@@ -11,26 +11,30 @@ class CategoryPageController extends Controller
 {
     public function view($id=null){
 
-        $articles = Article::orderBy('id','ASC')->paginate(9);
-        $articles->each(function($articles){
-            $articles->category;
-        });
-    
+        // $articles = Article::orderBy('id','ASC')->paginate(9);
+        // $articles->each(function($articles){
+        //     $articles->category;
+        // });
+
+        // $articles=categories()->articles()->map->id;
+        // $articles=App\Article::whereIn('id', $articles)->paginate(9);
 
         if($id and $id !='1') {
-    
+
+            $articles=category::findOrFail($id)->articles()->paginate(9); //nou
             $category= Category::find($id);
         }
         else{
 
-            
+            $articles = Article::orderBy('id','ASC')->paginate(9); //nou
             $category= Category::find(1);
             
         }
 
-        return view('category', ['category' => $category, 'articles' => $articles]);
+        return view('category', ['articles' => $articles, 'category' => $category]);
 
 
 
     }
 }
+
--- resources/views/elements/news.blade.php
+++ resources/views/elements/news.blade.php
@@ -1,6 +1,6 @@
 
 
-<div class="row">
+<!-- <div class="row">
 @foreach($articles as $article)
 
 @if($category->id == '1')
@@ -45,5 +45,32 @@
 <div class="pagination" style="max-width: 247.89px;">
 {!!$articles->render()!!}
 </div>
-</center>
+</center> -->
 
+
+<div class="row">
+@foreach($articles as $article)
+
+
+<div class="col-sm-4 my-4">
+    <div class="card">
+    <img class="card-img-top" src="{{ asset('images/' . $article->image) }}" alt="">
+    <div class="card-body">
+        <h4 class="card-title">{{$article->title}}</h4>
+        <p class="card-text">{{str_limit($article->content, 100)}}</p>
+        
+    </div>
+    <div class="card-footer">
+        <a href="{{ route('article', $article) }}" class="btn btn-danger">Read more</a>
+    </div>
+    </div>
+</div>
+
+
+@endforeach
+</div>
+<center>
+<div class="pagination" style="max-width: 247.89px;">
+{!!$articles->render()!!}
+</div>
+</center>
